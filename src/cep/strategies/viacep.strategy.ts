d8888ede5d3fe2731fc64ee8a97fa1d483c4b161
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-06-24 13:23:14 -0300

    Corrige erro de tipagem

--- src/cep/strategies/viacep.strategy.ts
+++ src/cep/strategies/viacep.strategy.ts
@@ -6,7 +6,15 @@ import { CepStrategy } from './cep-strategy.interface';
 @Injectable()
 export class ViaCepStrategy implements CepStrategy {
   async fetchCep(cep: string): Promise<CepResponseDto> {
-    const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    interface ViaCepResponse {
+      cep: string;
+      localidade: string;
+      uf: string;
+      erro?: boolean;
+      [key: string]: any;
+    }
+
+    const { data } = await axios.get<ViaCepResponse>(`https://viacep.com.br/ws/${cep}/json/`);
     if (data.erro) throw new HttpException('CEP não encontrado no ViaCEP', 404);
     return {
       cep: data.cep,
